bb019beba90bced1861e8e479b359a45564af753
Nelli <nelli.sedinkina.v@gmail.com>
2025-07-04 23:41:26 +0200

    hww 2

---
+++ src/server.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+export const startServer = () => {
+  const app = express();
+  const PORT = Number(getEnvVar('PORT', '3000'));
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    if (contacts.length === 0) {
+      res.status(404).json({
+        status: 404,
+        message: 'Contacts not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const getContact = await getContactById(contactId);
+    if (!getContact) {
+      res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: getContact,
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
