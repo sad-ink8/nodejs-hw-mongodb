df3d83dab97b6dded3a4e4432333bf7433c6a078
Nelli <nelli.sedinkina.v@gmail.com>
2025-07-08 23:33:14 +0200

    fix wrapper

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,7 +6,7 @@ import {
   patchContactController,
   deleteContactController,
 } from '../controllers/contacts.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
