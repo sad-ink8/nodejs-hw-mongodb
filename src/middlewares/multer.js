1874c2ba041d6c6ded4ef7a59677fc6513dc97b4
Nelli <nelli.sedinkina.v@gmail.com>
2025-07-12 23:05:57 +0200

    fix photos

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,9 +1,9 @@
 import multer from 'multer';
-import { TEMP_UPLOAD_DIR } from '../constants/index.js';
+import { TEMPLATES_DIR } from '../constants/index.js';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, TEMP_UPLOAD_DIR);
+    cb(null, TEMPLATES_DIR);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
